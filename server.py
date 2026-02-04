ee0241a45b82530cc59380f959dc570fe8570dda
rlawndud <42332990+rlawndud@users.noreply.github.com>
2026-02-04 13:26:04 +0900

    Update server.py

--- server.py
+++ server.py
@@ -2,9 +2,5 @@ import asyncio
 
 from websocketserver import WebsocketServer
 
-#10.101.152.35
-#220.90.180.89
-#192.168.0.45
-
 server = WebsocketServer(hostadr="0.0.0.0", port=8080)
 asyncio.run(server.run())
